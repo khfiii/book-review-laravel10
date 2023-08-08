990ae1e894fae64eb67e84e8e196c4d4a77291c7
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-08-08 08:30:58 +0600

    add query scopes to show highestrated books and popular book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,5 +12,22 @@ class Book extends Model
 
     public function reviews(){
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%'.$title.'%');
+    }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
     }
 }
