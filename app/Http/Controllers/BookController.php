b99c0a9daaeaae66803920a3cd198f2860922f50
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-08-10 20:35:05 +0600

    add rating and review count on all pages

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -26,11 +26,17 @@ class BookController extends Controller
             'popular_last_6months'=>$books->popularLast6Months(),
             'highest_rated_last_month'=>$books->highestRatedLastMonth(),
             'highest_rated_last_6months'=>$books->highestRatedLast6Months(),
-            default=>$books->latest()
+            default=>$books->latest()->withAvgRating()->withReviewsCount()
         };
 
         $cacheKey = 'books:'.$filter.':'.$title;
-        $books = cache()->remember($cacheKey, 3600, fn()=>$books->get());
+        $books =
+//            cache()->
+//            remember($cacheKey,
+//                3600,
+//                fn()=>
+                $books->get();
+//    );
         return view('books.index', ['books'=>$books]);
     }
 
@@ -53,13 +59,19 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Book $book)
+    public function show(int $id)
     {
 
-        $cacheKey = 'book:'.$book->id;
-        $book = cache()->remember($cacheKey, 3600, fn()=>$book->load(
-            ['reviews'=>fn($query) => $query->latest()]
-        ));
+        $cacheKey = 'book:'.$id;
+        $book = cache()->remember(
+            $cacheKey,
+            3600,
+            fn()=>
+            Book::with([
+                'reviews'=>fn($query) => $query->latest()
+                ])->withAvgRating()->withReviewsCount()->findOrFail($id));
+
+
         return view('books.show', ['book'=>$book]
         );
     }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder as QueryBuilder;
+use MongoDB\Driver\Query;
 
 class Book extends Model
 {
@@ -20,15 +21,25 @@ class Book extends Model
         return $query->where('title', 'LIKE', '%'.$title.'%');
     }
 
+    public function scopeWithReviewsCount(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withCount(['reviews'=> fn(Builder $q)=>$this->dateRangeFilter($q, $from, $to)]);
+    }
+
+    public function scopeWithAvgRating(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withAvg(['reviews'=> fn(Builder $q)=>$this->dateRangeFilter($q, $from, $to)], 'rating');
+    }
+
     public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder
     {
-        return $query->withCount(['reviews'=> fn(Builder $q)=>$this->dateRangeFilter($q, $from, $to)])
+        return $query->withReviewsCount()
             ->orderBy('reviews_count', 'desc');
     }
 
     public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder
     {
-        return $query->withAvg(['reviews'=> fn(Builder $q)=>$this->dateRangeFilter($q, $from, $to)], 'rating')
+        return $query->withAvgRating()
             ->orderBy('reviews_avg_rating', 'desc');
     }
 
@@ -67,5 +78,11 @@ class Book extends Model
         return $query ->highestRated(now()->subMonth(6), now())
             ->popular(now()->subMonth(6), now())
             ->minReviews(5);
+    }
+
+    protected static function booted(){
+        static::updated(fn (Book $book)=> cache()->forget('book:'.$book->id));
+        static::deleted(fn (Book $book)=> cache()->forget('book:'.$book->id));
+
     }
 }
