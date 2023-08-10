446130235394d2efc3c9603a7b4b034b8fa8f803
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-08-10 22:11:19 +0600

    add scoped resource controller - adding a review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -18,6 +18,7 @@ class Review extends Model
     protected static function booted(){
         static::updated(fn (Review $review)=> cache()->forget('book:'.$review->book_id));
         static::deleted(fn (Review $review)=> cache()->forget('book:'.$review->book_id));
+        static::created(fn (Review $review)=> cache()->forget('book:'.$review->book_id));
 
     }
 }
