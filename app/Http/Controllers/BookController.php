db44cbfcf5288ae5a814470c599ab08bb5716e19
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-08-10 19:39:05 +0600

    add cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
@@ -28,8 +29,8 @@ class BookController extends Controller
             default=>$books->latest()
         };
 
-        $books = $books->get();
-
+        $cacheKey = 'books:'.$filter.':'.$title;
+        $books = cache()->remember($cacheKey, 3600, fn()=>$books->get());
         return view('books.index', ['books'=>$books]);
     }
 
