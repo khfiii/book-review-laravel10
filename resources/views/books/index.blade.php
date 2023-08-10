a14673bf661ce0e7fad42f0dc72601dc008d32ea
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-08-10 11:02:43 +0600

    add filter books by title form

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,7 +3,12 @@
 @section('content')
     <h1 class="mb-10 text-2x1">Books</h1>
 
-    <form action=""></form>
+    <form method="GET" action="{{route('books.index')}}" class="mb-4 flex items-center space-x-2">
+        <input type="text" name="title" placeholder="Search by title"
+        value="{{ request('title') }}" class="input h-10">
+        <button type="submit" class="btn h-10">Search</button>
+        <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
+    </form>
 
     <ul> @forelse($books as $book)
             <li class="mb-4">
